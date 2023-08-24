b2bb5f37c70ebcb7dcc26169b45436668e73a725
Keerthanaakula2 <kits2001@gmail.com>
2023-08-24 12:33:31 -0400

    changes

--- tests/test_inventory_dag_pytest.py
+++ tests/test_inventory_dag_pytest.py
@@ -8,20 +8,4 @@ def dag_bag():
 
 @pytest.fixture
 def dag(dag_bag):
-    return dag_bag.get_dag('inventory_dag')
-
-def test_dag_load(dag_bag):
-    assert dag_bag.dags is not None
-    assert 'inventory_dag' in dag_bag.dags
-
-def test_task_count(dag):
-    assert len(dag.tasks) == 4
-
-def test_dependencies(dag):
-    tasks = dag.tasks
-    assert tasks[1].upstream_task_ids == ['start_task']
-    assert tasks[2].upstream_task_ids == ['ingest_inventory_data_task']
-    assert tasks[3].upstream_task_ids == ['analyze_inventory_task']
-
-def test_start_date(dag):
-    assert dag.start_date == days_ago(1)
+    return
---
+++ tests/test_inventory_dag_unittest.py
@@ -0,0 +1,25 @@
+import unittest
+from airflow.models import DagBag
+
+class TestInventoryManagementDAG(unittest.TestCase):
+
+    def setUp(self):
+        self.dagbag = DagBag(include_examples=False)
+        self.dag_id = 'inventory_dag'
+        self.dag = self.dagbag.get_dag(self.dag_id)
+
+    def test_dag_load(self):
+        self.assertNotEqual(self.dag, None)
+        self.assertEqual(self.dag.dag_id, self.dag_id)
+
+    def test_tasks(self):
+        task_ids = ['start_task', 'ingest_inventory_data_task', 'analyze_inventory_task', 'end_task']
+        for task_id in task_ids:
+            task = self.dag.get_task(task_id)
+            self.assertIsNotNone(task)
+            self.assertTrue(task.has_dag(self.dag))
+            self.assertTrue(task.upstream_task_ids, msg=f"{task_id} has no upstream tasks")
+            self.assertTrue(task.downstream_task_ids, msg=f"{task_id} has no downstream tasks")
+
+if __name__ == '__main__':
+    unittest.main()
